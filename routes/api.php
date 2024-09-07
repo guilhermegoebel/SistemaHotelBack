cac1d202c44d540ec6bb6cef8ee54e808c33d689
gabriel_shg <gabrielfelipeshigio@gmail.com>
2024-09-07 18:57:43 -0300

    Fixed api.php and salvar() functionalities

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservaController;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+}); //->middleware('auth:sanctum');
+
 Route::post('/reserva', [ReservaController::class, 'salvar']);
+
+Route::post('/test', function (Request $request) {
+    return response()->json(['message' => 'POST request successful']);
+});
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix('api')->group(base_path('routes/api.php'));
